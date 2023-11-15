8f29177fef14fd947a0a247912d17a790ee9f226
mintcoo <xhaktm3344@gmail.com>
2023-11-15 15:43:55 +0900

    수평 수직 정렬 스타일 등

--- Practice.md
+++ Practice.md
@@ -226,3 +226,51 @@ class App extends StatelessWidget {
 }
 ```
 
+## Flutter UI
+
+![image-20231115145107288](C:\Users\han\Desktop\FlutterPractice\assets\image-20231115145107288.png)
+
+```dart
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text("Hey, Cinna",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w800,
+                            )),
+                        Text("Welcome Back",
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 15,
+                            )),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
+// 여기까지 수평 수직 및 패딩, 정렬, 스타일등 배움
+```
+
--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -9,20 +9,38 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter!!"),
-          centerTitle: true,
-          backgroundColor: Colors.deepPurple,
-          shadowColor: Colors.red,
-          elevation: 10,
-        ),
-        body: Center(
-          child: Text(
-            "Hello WORLD",
-            style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-        ),
-      ),
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text("Hey, Cinna",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w800,
+                            )),
+                        Text("Welcome Back",
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 15,
+                            )),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
