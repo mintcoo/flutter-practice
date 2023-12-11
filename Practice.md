732eafa7428de592ca89e82c41891713981a34b8
mintcoo <xhaktm3344@gmail.com>
2023-12-11 22:34:08 +0900

    웹툰페이지와 연결

--- Practice.md
+++ Practice.md
@@ -2131,3 +2131,67 @@ builder: (context, snapshot) {
     이렇게 어떤 url종류인지 명시해준다
 ```
 
+```dart
+// episode widget
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webtoonapp/models/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+
+  final WebtoonEpisodeModel episode;
+  final String webtoonId;
+
+  void onButtonTap() async {
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 7),
+        decoration: BoxDecoration(
+            color: Colors.green.shade400,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 3,
+                offset: const Offset(3, 3),
+                color: Colors.black.withOpacity(0.5),
+              )
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const Icon(
+                Icons.arrow_circle_right_outlined,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+```
+
--- webtoonapp/lib/screens/detail_screen.dart
+++ webtoonapp/lib/screens/detail_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webtoonapp/models/webtoon_detail_model.dart';
 import 'package:webtoonapp/models/webtoon_episode_model.dart';
 
 import 'package:webtoonapp/models/webtoon_model.dart';
 import 'package:webtoonapp/services/api_service.dart';
+import 'package:webtoonapp/widgets/episode_widget.dart';
 
 class DetailWebtoon extends StatefulWidget {
   final WebtoonModel webtoon;
@@ -142,40 +144,9 @@ class _DetailWebtoonState extends State<DetailWebtoon> {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 7),
-                            decoration: BoxDecoration(
-                                color: Colors.green.shade400,
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: [
-                                  BoxShadow(
-                                    blurRadius: 3,
-                                    offset: const Offset(3, 3),
-                                    color: Colors.black.withOpacity(0.5),
-                                  )
-                                ]),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 7),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.arrow_circle_right_outlined,
-                                    color: Colors.white,
-                                  )
-                                ],
-                              ),
-                            ),
+                          Episode(
+                            episode: episode,
+                            webtoonId: widget.webtoon.id,
                           )
                       ],
                     );
